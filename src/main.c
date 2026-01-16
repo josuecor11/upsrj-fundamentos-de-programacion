ab6ddcabee70719fae3abc27973a45d1ce7962e7
josuecor11 <josuecor1101@gmail.com>
2026-01-16 11:29:02 -0600

    Primer entrega area y volumen cubo y cuadrado Josue Cornejo

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+float area(float l){
+    float are = l * l;
+    return are;
+}
+
+float volumen(float l){
+    float volume = l * l * l;
+    return volume;
+}
+
 int main(int argc, char *argv)
 {
-    printf("Hello world!\n");
+    float l = 0;
+    
+    printf("Ingrese la medida del lado del cuadrado\n");
+    scanf("%f", &l);
+    
+    float a = area(l);
+    float v = volumen(l);
+
+    printf("El area de el cuadrado es de : %f \nEl volumen del cubo es de : %f\n", a, v);
+
     return 0;
 }
